6b5c573b85015d2435e048b14a21bbba1afc4b2d
rfm <rfm.1991.cas@gmail.com>
2021-01-25 18:15:22 +0000

    Changes to script that creates locus graph.

--- locus_pairwise_graph.py
+++ locus_pairwise_graph.py
@@ -12,6 +12,7 @@ import subprocess
 
 import networkx as nx
 from Bio import SeqIO
+from Bio.Seq import Seq
 from pyvis.network import Network
 
 
@@ -106,7 +107,7 @@ def run_blast(blast_path, blast_db, fasta_file, blast_output,
     return stderr
 
 
-def graph_html(networkx_graph, output_html):
+def graph_html(networkx_graph, reps, output_html, title):
     """
     """
 
@@ -115,16 +116,32 @@ def graph_html(networkx_graph, output_html):
     # populates the nodes and edges data structures
     nt.from_nx(networkx_graph)
 
+    for node in reps:
+        nt.nodes[int(node)]['color'] = '#dd4b39'
+
+    nt.show_buttons(filter_=['physics'])
+    nt.heading = title
+
     nt.show(output_html)
 
 
 # add way to apply cutoff and output groups of alleles
-input_file = '/home/rfm/Desktop/rfm/Lab_Analyses/GAS_PrepExternalSchema/wgMLST_schema/spyogenes_schema/solve_problematic_2/scl_analysis/GCF-002236855-protein1020_protein.fasta'
-output_dir = '/home/rfm/Desktop/rfm/Lab_Analyses/GAS_PrepExternalSchema/wgMLST_schema/spyogenes_schema/solve_problematic_2/scl_analysis'
-def main(input_file, output_dir):
+#locus_id = 'GCF-002236855-protein1020'
+#schema_dir = '/home/rfm/Desktop/rfm/Lab_Analyses/GAS_PrepExternalSchema/wgMLST_schema/spyogenes_schema/solve_problematic_2/spyogenes_schema_processed'
+#output_dir = '/home/rfm/Desktop/rfm/Lab_Analyses/GAS_PrepExternalSchema/wgMLST_schema/spyogenes_schema/solve_problematic_2/scl_analysis'
+#blast_score_ratio = 0.6
+def main(locus_id, schema_dir, output_dir, blast_score_ratio):
+
+    locus_file = os.path.join(schema_dir, locus_id+'.fasta')
 
     # create file with short ids
-    records = [((rec.id).split('_')[-1], str(rec.seq)) for rec in SeqIO.parse(input_file, 'fasta')]
+    records = [((rec.id).split('_')[-1], str((rec.translate(table=11, cds=True)).seq))
+               for rec in SeqIO.parse(locus_file, 'fasta')]
+
+    # get locus rep IDs
+    locus_rep_file = os.path.join(schema_dir, 'short', locus_id+'_short.fasta')
+    reps = [(rec.id).split('_')[-1] for rec in SeqIO.parse(locus_rep_file, 'fasta')]
+
     # save records with short ids
     records_str = '\n'.join(['>{0}\n{1}'.format(*rec) for rec in records])
 
@@ -152,6 +169,9 @@ def main(input_file, output_dir):
     # exclude self hits from blast results
     blast_results_filtered = [l for l in blast_results if l[0] != l[1]]
 
+    # exclude based on evalue
+    blast_results_filtered = [l for l in blast_results_filtered if float(l[10]) <= 10**-13]
+
     # compute and add BSR
     for i in range(len(blast_results_filtered)):
         query = blast_results_filtered[i][0]
@@ -159,36 +179,46 @@ def main(input_file, output_dir):
         bsr = float(score) / float(self_scores[query])
         blast_results_filtered[i].append(bsr)
 
+    # exclude based on BSR
+    blast_results_filtered = [l for l in blast_results_filtered if float(l[-1]) >= blast_score_ratio]
+
     # instantiate graph
     G = nx.Graph()
 
-    # get nodes and add to graph
-    #nodes = list(set([l[0] for l in blast_results_filtered]))
-    #G.add_nodes_from(nodes)
-
-    edges = [(l[0], l[1]) for l in blast_results_filtered if float(l[-1]) >= 0.8]
-    G.add_edges_from(edges)
+    # add edges with weight
+    edges = [(l[0], l[1], l[-1]) for l in blast_results_filtered]
+    G.add_weighted_edges_from(edges)
 
     # create HTML with grpah viz
-    output_html = os.path.join(output_dir, 'nx.html')
-    graph_html(G, output_html)
+    output_html = os.path.join(output_dir, '{0}_graph.html'.format(locus_id))
+    graph_html(G, reps, output_html, locus_id)
 
 
+# add option to scan for motifs in alleles and color nodes based on that information
 def parse_arguments():
 
     parser = argparse.ArgumentParser(description=__doc__,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
 
-    parser.add_argument('-i', type=str, required=True,
-                        dest='input_file',
+    parser.add_argument('-l', type=str, required=True,
+                        dest='locus_id',
+                        help='')
+
+    parser.add_argument('-s', type=str, required=True,
+                        dest='schema_dir',
                         help='')
 
     parser.add_argument('-o', type=str, required=True,
                         dest='output_dir',
                         help='')
 
+    parser.add_argument('--bsr', type=float, required=False,
+                        default=0.6,
+                        dest='blast_score_ratio',
+                        help='')
+
     args = parser.parse_args()
-    
+
     return args
 
 
