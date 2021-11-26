5e84975eea7f58913a3a4c7cef50b145b4eff51b
rfm <rfm.1991.cas@gmail.com>
2021-11-26 19:57:27 +0000

    Optimizations in locus_length_variability.py

--- alleles_frequency.py
+++ alleles_frequency.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Purpose
--------
-This module computes the frequency for alleles in a locus
-based on allele calling results. It also determines the
-number of distinct protein variants and the list of alleles
-that code for each protein variant.
 
-Code documentation
-------------------
+@author: rfm
 """
 
 
@@ -22,44 +15,45 @@ from Bio.Seq import Seq
 
 
 def read_tabular(input_file, delimiter='\t'):
-    """ Read a TSV file.
+    """ Read tabular file.
 
-    Parameters
-    ----------
-    input_file : str
-        Path to a TSV file.
-    delimiter : str
-        Delimiter used to separate file columns.
+        Parameters
+        ----------
+        input_file : str
+            Path to a tabular file.
+        delimiter : str
+            Delimiter used to separate file fields.
 
-    Returns
-    -------
-    lines : list
-        A list with a sublist per line in the input file.
-        Each sublist has the fields that were separated by
-        the defined delimiter.
+        Returns
+        -------
+        lines : list
+            A list with a sublist per line in the input file.
+            Each sublist has the fields that were separated by
+            the defined delimiter.
     """
 
     with open(input_file, 'r') as infile:
-        lines = list(csv.reader(infile, delimiter=delimiter))
+        reader = csv.reader(infile, delimiter=delimiter)
+        lines = [line for line in reader]
 
     return lines
 
 
-def write_to_file(text, output_file, write_mode, end_char='\n'):
+def write_to_file(text, output_file, write_mode, end_char):
     """ Writes a single string to a file.
 
-    Parameters
-    ----------
-    text : str
-        A single string to write to the output file.
-    output_file : str
-        Path to the output file.
-    write_mode : str
-        Write mode can be 'w', writes text and overwrites
-        any text in file, or 'a', appends text to text
-        already in file.
-    end_char : str
-        Character added to the end of the file.
+        Parameters
+        ----------
+        text : str
+            A single string to write to the output file.
+        output_file : str
+            Path to the output file.
+        write_mode : str
+            Write mode can be 'w', writes text and overwrites
+            any text in file, or 'a', appends text to text
+            already in file.
+        end_char : str
+            Character added to the end of the file.
     """
 
     with open(output_file, write_mode) as out:
@@ -71,35 +65,30 @@ def write_lines(lines, output_file):
         are joined with newlines before being written to
         file.
 
-    Parameters
-    ----------
-    lines : list
-        List with the lines/strings to write to the
-        output file.
-    output_file : str
-        Path to the output file.
+        Parameters
+        ----------
+        lines : list
+            List with the lines/strings to write to the
+            output file.
+        output_file : str
+            Path to the output file.
     """
 
     joined_lines = '\n'.join(lines)
 
-    write_to_file(joined_lines, output_file, 'a')
+    write_to_file(joined_lines, output_file, 'a', '\n')
 
 
 def translate_sequence(dna_str, table_id):
     """ Translate a DNA sequence using the BioPython package.
 
-    Parameters
-    ----------
-    dna_str : str
-        DNA sequence as string type.
-    table_id : int
-        Translation table identifier.
+        Args:
+            dna_str (str): DNA sequence as string type.
+            table_id (int): translation table identifier.
 
-    Returns
-    -------
-    protseq : str
-        Protein sequence created by translating
-        the input DNA sequence.
+        Returns:
+            protseq (str): protein sequence created by translating
+            the input DNA sequence.
     """
 
     myseq_obj = Seq(dna_str)
@@ -112,17 +101,17 @@ def translate_sequence(dna_str, table_id):
 def group_by_protein(fasta_file):
     """ Groups DNA sequences based on the protein they code for.
 
-    Parameters
-    ----------
-    fasta_file : str
-        Path to a FASTA file with DNA sequences.
+        Parameters
+        ----------
+        fasta_file : str
+            Path to the FASTA file with DNA sequences.
 
-    Returns
-    -------
-    protein_diversity : dict
-        Dictionary with protein sequences as keys and a
-        list as value for each key. Each list has the allele
-        identifiers of the alleles that code for the protein.
+        Returns
+        -------
+        protein_diversity : dict
+            Dictionary with protein sequences as keys and a
+            list as value for each key. Each list has the allele
+            identifiers of the alleles that code for the protein.
     """
 
     protein_diversity = {}
@@ -132,12 +121,9 @@ def group_by_protein(fasta_file):
         sequence = str(record.seq)
         try:
             protein = str(translate_sequence(sequence, 11))
-        # alleles in chewie schemas should be translatable
         except Exception:
             continue
 
-        # store the allele identifiers for alleles that code for
-        # each distinct protein
         protein_diversity.setdefault(protein, []).append(allele_id)
 
     return protein_diversity
@@ -148,28 +134,28 @@ def protein_frequencies(protein_groups, locus_classifications):
         and determines the frequency of each protein
         based on the alleles that code for that protein.
 
-    Parameters
-    ----------
-    protein_groups : dict
-        Dictionary with protein sequences as keys and
-        a list with the IDs of the alleles that code
-        for the protein as value.
-    locus_classifications : dict
-        A dictionary with allele IDs as keys and a list
-        with the sample IDs that have the allele and the
-        frequency of the allele based on the results in
-        the allele calling matrix.
+        Parameters
+        ----------
+        protein_groups : dict
+            Dictionary with protein sequences as keys and
+            a list with the IDs of the alleles that code
+            for the protein.
+        locus_classification : dict
+            A dictionary with alleles IDs as keys and a list
+            with the sample IDs that have the allele and the
+            frequency of the allele based on the results in
+            a AlleleCall matrix.
 
-    Returns
-    -------
-    protein_lines : list
-        List with Fasta string records (e.g.: '>1\nMKT').
-    prots_freqs : list
-        List with one sublist per distinct protein coded
-        by the alleles of the locus. Each sublist has
-        the protein ID attributed to the protein, the
-        frequency of the protein and a list with the IDs
-        of the alleles that code for that protein.
+        Returns
+        -------
+        protein_lines : list
+            List with Fasta string records (e.g.: '>1\nMKT').
+        prots_freqs : list
+            List with one sublist per distinct protein coded
+            by the alleles of the locus. Each sublist has
+            the protein ID attributed to the protein, the
+            frequency of the protein and a list with the IDs
+            of the alleles that code for that protein.
     """
 
     protid = 1
@@ -177,12 +163,9 @@ def protein_frequencies(protein_groups, locus_classifications):
     prots_freqs = []
     for k, v in protein_groups.items():
         line = '>{0}\n{1}'.format(protid, k)
-        # store protein record
         protein_lines.append(line)
         total_freq = 0
         for a in v:
-            # increment protein frequency for each allele
-            # that codes for that protein
             total_freq += locus_classifications[int(a)][-1]
         prots_freqs.append([protid, total_freq, v])
         protid += 1
@@ -198,30 +181,20 @@ def main(schema, locus_id, allelecall_matrix, output_dir):
     # import AlleleCall matrix
     profiles = read_tabular(allelecall_matrix)
 
-    locus_id = locus_id.split('.fasta')[0]
-
     # get locus column
-    try:
-        locus_index = profiles[0].index(locus_id)
-    # locus identifier does not have ".fasta" extension
-    except Exception as e:
-        locus_index = profiles[0].index(locus_id+'.fasta')
+    locus_index = profiles[0].index(locus_id+'.fasta')
 
     # get locus classifications for all genomes
     locus_classifications = {}
     for line in profiles[1:]:
         allele_id = line[locus_index]
-        # remove "INF-" prefix
         if 'INF-' in allele_id:
             allele_id = allele_id.replace('INF-', '')
-        # get allele identifier and identifiers for samples with that allele
+
         try:
             allele_id = int(allele_id)
-            # do not store missing data that has been converted to "0"
-            if allele_id != 0:
-                sample_id = line[0]
-                locus_classifications.setdefault(allele_id, []).append(sample_id)
-        # missing data such as "LNF" cannot be converted to integer
+            sample_id = '.'.join(line[0].split('.')[:-1])
+            locus_classifications.setdefault(allele_id, []).append(sample_id)
         except ValueError:
             pass
 
@@ -245,14 +218,11 @@ def main(schema, locus_id, allelecall_matrix, output_dir):
     protein_lines, prots_freqs = protein_frequencies(protein_groups, locus_classifications)
 
     # write Fasta with proteins
-    protein_file = os.path.join(output_dir,
-                                '{0}_protein_variants.fasta'.format(locus_id))
+    protein_file = os.path.join(output_dir, '{0}_protein.fasta'.format(locus_id))
     write_lines(protein_lines, protein_file)
 
     # create output file with protein frequencies
-    prots_freqs = sorted(prots_freqs,
-                         key=lambda x: x[1],
-                         reverse=True)
+    prots_freqs = sorted(prots_freqs, key=lambda x: x[1], reverse=True)
     prots_freqs_lines = ['{0}\t{1}\t{2}'.format(l[0], l[1], ','.join(l[2]))
                          for l in prots_freqs]
     prots_freqs_header = 'protein_id\tfrequency\talleles_ids'
@@ -262,19 +232,15 @@ def main(schema, locus_id, allelecall_matrix, output_dir):
     write_lines(prots_freqs_lines, protein_freqs_file)
 
     # create output file with DNA sequences frequencies
-    output_lines = [[k, v[-1], v[:-1]]
-                    for k, v in locus_classifications.items()]
+    output_lines = [[k, v[-1], v[:-1]] for k, v in locus_classifications.items()]
     # sort based on decreased frequency
-    output_lines = sorted(output_lines,
-                          key=lambda x: x[1],
-                          reverse=True)
+    output_lines = sorted(output_lines, key=lambda x: x[1], reverse=True)
     output_header = 'allele_id\tfrequency\tsamples'
     output_text = ['{0}\t{1}\t{2}'.format(l[0], l[1], ','.join(l[2]))
                    for l in output_lines]
     output_text = [output_header] + output_text
 
-    output_file = os.path.join(output_dir,
-                               '{0}_allele_frequencies.tsv'.format(locus_id))
+    output_file = os.path.join(output_dir, '{0}_frequencies.tsv'.format(locus_id))
     write_lines(output_text, output_file)
 
 
@@ -285,20 +251,19 @@ def parse_arguments():
 
     parser.add_argument('-s', type=str, required=True,
                         dest='schema',
-                        help='Path to the schema directory.')
+                        help='')
 
     parser.add_argument('-l', type=str, required=True,
                         dest='locus_id',
-                        help='Locus identifier (without ".fasta" extension).')
+                        help='')
 
     parser.add_argument('-m', type=str, required=True,
                         dest='allelecall_matrix',
-                        help='Path to file that contains a matrix with '
-                             'allele calling results.')
+                        help='')
 
     parser.add_argument('-o', type=str, required=True,
                         dest='output_dir',
-                        help='Path to output directory.')
+                        help='')
 
     args = parser.parse_args()
 
--- locus_length_variability.py
+++ locus_length_variability.py
@@ -3,7 +3,10 @@
 """
 Purpose
 -------
-
+This scripts computes loci allele length statistics for all
+loci in a schema, identifying loci with multiple allele
+length modes and loci with alleles that deviate from the
+mode value.
 
 Code documentation
 ------------------
@@ -24,16 +27,16 @@ from plotly.subplots import make_subplots
 def import_sequences(fasta_path):
     """ Imports sequences from a FASTA file.
 
-        Parameters
-        ----------
-        fasta_path : str
-            Path to a FASTA file.
+    Parameters
+    ----------
+    fasta_path : str
+        Path to a FASTA file.
 
-        Returns
-        -------
-        seqs_dict : dict
-            Dictionary that has sequences ids as keys and
-            sequences as values.
+    Returns
+    -------
+    seqs_dict : dict
+        Dictionary that has sequence identifiers as keys and
+        sequences as values.
     """
 
     records = SeqIO.parse(fasta_path, 'fasta')
@@ -43,7 +46,23 @@ def import_sequences(fasta_path):
 
 
 def histogram_tracer(xdata, ydata, marker_color):
-    """
+    """ Creates a histogram (go.Bar) trace.
+
+    Parameters
+    ----------
+    xdata : list
+        Sets the x coordinates (sequence
+        length values).
+    ydata : list
+        Sets the y coordinates (counts for
+        each sequence length value).
+    marker_color : str
+        Color to fill the bars.
+
+    Retunrs
+    -------
+    trace : plotly.graph_objects.Bar
+        Trace with data to display histogram.
     """
 
     trace = go.Bar(x=xdata,
@@ -71,9 +90,8 @@ def add_multiple_traces(traces, fig, srow, scol, max_cols):
 
     row_start = srow
     col_start = scol
-    for k, v in traces.items():
-        trace = v[1]
-        fig.append_trace(trace, row_start, col_start)
+    for t in traces:
+        fig.append_trace(t, row_start, col_start)
         if col_start < max_cols:
             col_start += 1
         else:
@@ -84,7 +102,16 @@ def add_multiple_traces(traces, fig, srow, scol, max_cols):
 
 
 def write_text(text, output_file, end='\n'):
-    """
+    """ Writes text to a file.
+
+    Parameters
+    ----------
+    text : str
+        Text to write.
+    output_file : str
+        Path to output file.
+    end : str
+        Characters to add to end of file.
     """
 
     with open(output_file, 'w') as outfile:
@@ -97,17 +124,15 @@ def write_stats(stats, header, output_file):
 
     lines = [header]
     for locus, values in stats.items():
-        locus_id = (os.path.basename(locus)).split('.fasta')[0]
-        most_frequent_str = ';'.join([str(v) for v in values[-1]])
-        str_values = [str(v) for v in values[0:-1]] + [most_frequent_str]
-        current = [locus_id] + str_values
-        lines.append('\t'.join(current))
+        most_frequent_str = ';'.join([str(v) for v in values[4]])
+        str_values = [values[5]] + [str(v) for v in values[0:4]] + [most_frequent_str] + [values[7]]
+        lines.append('\t'.join(str_values))
 
     text = '\n'.join(lines)
     write_text(text, output_file)
 
 
-def main(schema_directory, output_directory):
+def main(schema_directory, output_directory, size_threshold):
 
     if os.path.isdir(output_directory) is not True:
         os.mkdir(output_directory)
@@ -119,13 +144,7 @@ def main(schema_directory, output_directory):
     schema_loci = [os.path.join(schema_directory, file)
                    for file in schema_loci]
 
-    full_stats = {}
-    single_mode_ids = []
-    single_mode_stats = {}
-    single_mode_tracer = {}
-    multi_mode_ids = []
-    multi_mode_stats = {}
-    multi_mode_tracer = {}
+    loci_stats = {}
     for locus in schema_loci:
         records = import_sequences(locus)
 
@@ -136,17 +155,21 @@ def main(schema_directory, output_directory):
         allele_lengths = {recid: len(seq) for recid, seq in records.items()}
         lengths_list = list(allele_lengths.values())
 
-        # determine mean
+        locus_id = os.path.basename(locus).split('.fasta')[0]
+
+        # determine sequence length mean
         locus_mean = round(statistics.mean(lengths_list), 1)
 
-        # determine median
+        # determine sequence length median
         locus_median = round(statistics.median(lengths_list), 1)
 
         # determine mode and create histograms for loci with single mode
         try:
             locus_mode = statistics.mode(lengths_list)
+        # locus does not have a unique mode
         except Exception as e:
-            print(locus, e)
+            locus_id = os.path.basename(locus).split('.fasta')[0]
+            print(locus_id, '\n', e)
             locus_mode = 'None'
 
         # determine if there are other values that are very frequent
@@ -160,9 +183,9 @@ def main(schema_directory, output_directory):
                      if v[1] >= (0.3*most_frequent[1])]
         frequent_values = [most_frequent] + high_freq
 
-        # select length values that are also very frequent
-        asm = most_frequent[0]-(most_frequent[0]*0.2)
-        alm = most_frequent[0]+(most_frequent[0]*0.2)
+        # determine if there are alleles outside (mode +/- (mode*size_threshold))
+        asm = most_frequent[0]-(most_frequent[0]*size_threshold)
+        alm = most_frequent[0]+(most_frequent[0]*size_threshold)
         outlier = [v
                    for v in freq_ordered[1:]
                    if (v[0] >= alm or v[0] <= asm)]
@@ -170,84 +193,76 @@ def main(schema_directory, output_directory):
         xdata = [v[0] for v in freq_ordered]
         ydata = [v[1] for v in freq_ordered]
 
-        if locus_mode != 'None' and len(outlier) > 0:
+        loci_stats[locus] = [total_alleles, locus_mean,
+                             locus_median, locus_mode,
+                             frequent_values, locus_id]
+
+        if locus_mode == 'None' or len(outlier) > 0:
             # create histogram object
             trace = histogram_tracer(xdata, ydata, '#0868ac')
-            single_mode_tracer[locus] = [frequent_values, trace]
-            single_mode_ids.append((os.path.basename(locus)).split('.fasta')[0])
-            # add data to dict
-            single_mode_stats[locus] = [total_alleles, locus_mean,
-                                        locus_median, locus_mode,
-                                        frequent_values]
-        elif locus_mode == 'None':
-            trace = histogram_tracer(xdata, ydata, '#0868ac')
-            multi_mode_tracer[locus] = [frequent_values, trace]
-            multi_mode_ids.append((os.path.basename(locus)).split('.fasta')[0])
-            # add data to dict
-            multi_mode_stats[locus] = [total_alleles, locus_mean, locus_median,
-                                       locus_mode, frequent_values]
-
-        full_stats[locus] = [total_alleles, locus_mean,
-                             locus_median, locus_mode,
-                             frequent_values]
+            if locus_mode == 'None':
+                loci_stats[locus].extend([trace, 'multimodal'])
+            elif len(outlier) > 0:
+                loci_stats[locus].extend([trace, 'outlier'])
+        else:
+            loci_stats[locus].extend([None, 'single'])
 
     # header for file with statistics
-    header = 'locus\tnum_alleles\tmean\tmedian\tmode\tmost_frequent'
+    header = 'locus\tnum_alleles\tmean\tmedian\tmode\tmost_frequent\tcategory'
     # template for file path
     file_template = '{0}/{1}'
 
     # save full stats
-    full_stats_outfile = file_template.format(output_directory,
-                                              'full_stats.tsv')
-    write_stats(full_stats, header, full_stats_outfile)
+    stats_outfile = file_template.format(output_directory,
+                                         'loci_stats.tsv')
+    write_stats(loci_stats, header, stats_outfile)
 
-    # create TSV lines with single mode loci data
-    single_mode_outfile = file_template.format(output_directory,
-                                               'single_mode_stats.tsv')
+    # create HTML with subplots for loci with alleles outside length threshold
+    # get list of loci
+    outlier_loci = [v[5]
+                    for k, v in loci_stats.items()
+                    if v[-1] == 'outlier']
 
-    write_stats(single_mode_stats, header, single_mode_outfile)
+    # get traces
+    outlier_traces = [v[-2]
+                      for k, v in loci_stats.items()
+                      if v[-1] == 'outlier']
 
-    # write single mode high length variability identifiers to file
-    single_mode_ids_outfile = file_template.format(output_directory,
-                                                   'single_mode_ids.txt')
-    single_mode_ids_text = '\n'.join(single_mode_ids)
-    write_text(single_mode_ids_text, single_mode_ids_outfile)
+    # create Figure object for subplots
+    outlier_mode_fig = create_subplots(len(outlier_traces), 2, outlier_loci)
+    outlier_mode_fig = add_multiple_traces(outlier_traces, outlier_mode_fig,
+                                           1, 1, 2)
 
-    # create HTML with subplots for loci with single mode
-    titles = [(os.path.basename(k)).split('.fasta')[0] for k in single_mode_tracer]
-    single_mode_fig = create_subplots(len(single_mode_tracer), 2, titles)
-    single_mode_fig = add_multiple_traces(single_mode_tracer, single_mode_fig,
-                                          1, 1, 2)
+    # adjust figure height and layout
+    fig_height = (statistics.math.ceil(len(outlier_mode_fig.data)/2))*300
+    outlier_mode_fig.update_layout(title='Loci with alleles 20% smaller or '
+                                         'larger than the allele length mode',
+                                   height=fig_height,
+                                   showlegend=False,
+                                   bargap=0.1,
+                                   template='ggplot2')
 
-    fig_height = (statistics.math.ceil(len(single_mode_fig.data)/2))*300
-    single_mode_fig.update_layout(title='Loci with alleles 20% smaller or larger than the allele length mode',
-                                  height=fig_height,
-                                  showlegend=False,
-                                  bargap=0.1,
-                                  template='ggplot2')
+    # log transform axes for more compact scaling
+    outlier_mode_fig.update_xaxes(type='log',
+                                  title_text='Allele length (log)')
+    outlier_mode_fig.update_yaxes(type='log',
+                                  title_text='Number of alleles (log)')
 
-    single_mode_fig.update_xaxes(type='log', title_text='Allele length (log)')
-    single_mode_fig.update_yaxes(type='log', title_text='Number of alleles (log)')
-
-    single_mode_plotfile = file_template.format(output_directory,
-                                                'single_mode.html')
-    plot(single_mode_fig, filename=single_mode_plotfile, auto_open=True)
-
-    # create TSV lines with multi mode loci data
-    multi_mode_outfile = file_template.format(output_directory,
-                                              'multi_mode_stats.tsv')
-    write_stats(multi_mode_stats, header, multi_mode_outfile)
-
-    # write multi mode identifiers to file
-    multi_mode_ids_outfile = file_template.format(output_directory,
-                                                  'multi_mode_ids.txt')
-    multi_mode_ids_text = '\n'.join(multi_mode_ids)
-    write_text(multi_mode_ids_text, multi_mode_ids_outfile)
+    outlier_mode_plotfile = file_template.format(output_directory,
+                                                 'outlier.html')
+    plot(outlier_mode_fig, filename=outlier_mode_plotfile, auto_open=False)
 
     # create HTML with subplots for loci with multiple modes
-    titles = [(os.path.basename(k)).split('.fasta')[0] for k in multi_mode_tracer]
-    multimodal_fig = create_subplots(len(multi_mode_tracer), 2, titles)
-    multimodal_fig = add_multiple_traces(multi_mode_tracer, multimodal_fig,
+    multimodal_loci = [v[5]
+                       for k, v in loci_stats.items()
+                       if v[-1] == 'multimodal']
+
+    multimodal_traces = [v[-2]
+                         for k, v in loci_stats.items()
+                         if v[-1] == 'multimodal']
+
+    multimodal_fig = create_subplots(len(multimodal_traces), 2, multimodal_loci)
+    multimodal_fig = add_multiple_traces(multimodal_traces, multimodal_fig,
                                          1, 1, 2)
 
     fig_height = (statistics.math.ceil(len(multimodal_fig.data)/2))*300
@@ -261,8 +276,8 @@ def main(schema_directory, output_directory):
     multimodal_fig.update_yaxes(type='log', title_text='Number of alleles (log)')
 
     plot_file = file_template.format(output_directory,
-                                     'multiple_mode.html')
-    plot(multimodal_fig, filename=plot_file, auto_open=True)
+                                     'multimodal.html')
+    plot(multimodal_fig, filename=plot_file, auto_open=False)
 
 
 def parse_arguments():
@@ -270,13 +285,21 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description=__doc__,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
 
-    parser.add_argument('-s', type=str, required=True,
+    parser.add_argument('-s', '--schema-directory', type=str,
+                        required=True,
                         dest='schema_directory',
-                        help='')
+                        help='Path to the schema\'s directory.')
 
-    parser.add_argument('-o', type=str, required=True,
+    parser.add_argument('-o', '--output-directory', type=str,
+                        required=True,
                         dest='output_directory',
-                        help='')
+                        help='Path to the output directory.')
+    
+    parser.add_argument('-s', '--size-threshold', type=float,
+                        required=False, default=0.2,
+                        dest='size_threshold',
+                        help='Locus length mode variation '
+                             'threshold for outliers.')
 
     args = parser.parse_args()
 
