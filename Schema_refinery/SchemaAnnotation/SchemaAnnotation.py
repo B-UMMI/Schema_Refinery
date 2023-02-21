323d4ad869c41dcbede06c0e4f431ed021624e3b
IgnesA <inesalmeida0987654@gmail.com>
2023-02-21 10:40:10 +0000

    remove unnecessary output_directory checks

--- Schema_refinery/SchemaAnnotation/SchemaAnnotation.py
+++ Schema_refinery/SchemaAnnotation/SchemaAnnotation.py
@@ -25,20 +25,16 @@ except ModuleNotFoundError:
     from Schema_refinery.SchemaAnnotation.ProteomeAnnotations import proteome_annotations
     from Schema_refinery.SchemaAnnotation.AnnotationMerger import annotation_merger
 
-def check_uniprot_arguments(uniprot_species, output_directory):
+def check_uniprot_arguments(uniprot_species):
 
     necessary_arguments = {
         "uniprot_species": "\tUniprot annotations need a species file argument, outputted by Uniprot Finder. -ps",
-        "output_directory": "\tUniprot annotations need an output directory argument. -o"
     }
 
     missing_arguments = []
 
     if not uniprot_species:
         missing_arguments.append("uniprot_species")
-
-    if not output_directory:
-        missing_arguments.append("output_directory")
 
     if len(missing_arguments > 0):
         print("\nError: ")
@@ -54,10 +50,10 @@ def schema_annotation(args):
         trembl, swiss =proteome_annotations(args.input_table, args.proteomes_directory, args.threads, args.retry, args.schema_directory, args.output_directory, args.cpu_cores)
 
     if 'uniprot' in args.annotation_options:
-        check_uniprot_arguments(args.species, args.output_directory)
+        check_uniprot_arguments(args.species)
 
     if 'matchSchemas' in args.annotation_options:
-        check_match_schemas_arguments(args.query_schema, args.subject_schema, args.old_schema_columns, args.match_to_add, args.output_directory)
+        check_match_schemas_arguments(args.query_schema, args.subject_schema, args.old_schema_columns, args.match_to_add)
         matched_schemas = match_schemas(args.query_schema, args.subject_schema, args.output_directory, args.blast_score_ratio, args.cpu_cores)
 
     annotation_merger(args.uniprot_species, args.uniprot_genus, genbank_file, trembl, swiss, args.match_to_add, args.old_schema_columns, matched_schemas, args.output_directory)
