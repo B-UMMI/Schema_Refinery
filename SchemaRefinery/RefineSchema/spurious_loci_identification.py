6c09a21791d0ab41bfdde2d64cda1fa5119cc890
IgnesA <inesalmeida0987654@gmail.com>
2023-05-11 10:23:16 +0100

    copy the functions for protein translation

--- SchemaRefinery/RefineSchema/spurious_loci_identification.py
+++ SchemaRefinery/RefineSchema/spurious_loci_identification.py
@@ -1,15 +1,128 @@
 import os
 import subprocess
+from Bio.Seq import Seq
 
 LOCUS_CLASSIFICATIONS_TO_CHECK = ["ASM", 
                                   #"ALM", 
-                                #   "NIPH", 
-                                #   "NIPHEM"
+                                  #"NIPH", 
+                                  #"NIPHEM"
                                   ]
+
+DNA_BASES = 'AGCT'
 
 def check_and_make_directory(dir:str):
     if not os.path.isdir(dir):
         os.mkdir(dir)
+
+def check_str_alphabet(input_string, alphabet):
+    alphabet_chars = set(alphabet)
+    string_chars = set(input_string)
+
+    diff = string_chars - alphabet_chars
+
+    return len(diff) == 0
+
+def check_str_multiple(input_string, number):
+    return (len(input_string) % number) == 0
+
+def reverse_str(input_string):
+    revstr = input_string[::-1]
+
+    return revstr
+
+def reverse_complement(input_string, alphabet):
+    translation_table = str.maketrans(alphabet, alphabet[::-1])
+
+    upper_string = input_string.upper()
+    complement_string = upper_string.translate(translation_table)
+    reverse_complement_string = reverse_str(complement_string)
+
+    return reverse_complement_string
+
+def translate_sequence(dna_str, table_id):
+    myseq_obj = Seq(dna_str)
+    protseq = Seq.translate(myseq_obj, table=table_id, cds=True)
+
+    return protseq
+
+def translate_dna_aux(dna_sequence, method, table_id):
+    myseq = dna_sequence
+    # try to translate original sequence
+    if method == 'original':
+        try:
+            protseq = translate_sequence(myseq, table_id)
+        except Exception as argh:
+            return argh
+    # try to translate the reverse complement
+    elif method == 'revcomp':
+        try:
+            myseq = reverse_complement(myseq, DNA_BASES)
+            protseq = translate_sequence(myseq, table_id)
+        except Exception as argh:
+            return argh
+    # try to translate the reverse
+    elif method == 'rev':
+        try:
+            myseq = reverse_str(myseq)
+            protseq = translate_sequence(myseq, table_id)
+        except Exception as argh:
+            return argh
+    # try to translate the reverse reverse complement
+    elif method == 'revrevcomp':
+        try:
+            myseq = reverse_str(myseq)
+            myseq = reverse_complement(myseq, DNA_BASES)
+            protseq = translate_sequence(myseq, table_id)
+        except Exception as argh:
+            return argh
+
+    return [protseq, myseq]
+
+def translate_dna(dna_sequence, table_id, min_len):
+    original_seq = dna_sequence.upper()
+    exception_collector = []
+    strands = ['sense', 'antisense', 'revsense', 'revantisense']
+    translating_methods = ['original', 'revcomp', 'rev', 'revrevcomp']
+
+    # check if the sequence has ambiguous bases
+    valid_dna = check_str_alphabet(original_seq, DNA_BASES)
+    if valid_dna is not True:
+        return 'ambiguous or invalid characters'
+
+    # check if sequence size is multiple of three
+    valid_length = check_str_multiple(original_seq, 3)
+    if valid_length is not True:
+        return 'sequence length is not a multiple of 3'
+
+    # check if sequence is not shorter than the accepted minimum length
+    if len(original_seq) < min_len:
+        return 'sequence shorter than {0} nucleotides'.format(min_len)
+
+    # try to translate in 4 different orientations
+    # or reach the conclusion that the sequence cannot be translated
+    i = 0
+    translated = False
+    while translated is False:
+        translated_seq = translate_dna_aux(original_seq, translating_methods[i], table_id)
+        if not isinstance(translated_seq, list):
+            exception_collector.append('{0}({1})'.format(strands[i],
+                                                         translated_seq.args[0]))
+
+        i += 1
+        if i == len(strands) or isinstance(translated_seq, list) is True:
+            translated = True
+
+    coding_strand = strands[i-1]
+
+    # if the sequence could be translated, return list with protein and DNA
+    # sequence in correct orientation
+    if isinstance(translated_seq, list):
+        return [translated_seq, coding_strand]
+    # if it could not be translated, return the string with all exception
+    # that were collected
+    else:
+        exception_str = ','.join(exception_collector)
+        return exception_str
 
 def run_blast_for_CDS(locus, classification, subjects_number, blast_results_dir, representative_file, current_cds_file):
     blast_results_file = os.path.join(blast_results_dir, f"blast_results_{locus}_{classification}.txt")
