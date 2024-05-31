9a00bd3aef2fa0a625d230b015a0283a3ffb06d7
MForofontov <mykyta.forofontov@hotmail.com>
2024-05-31 09:43:46 +0100

    docs: Documented function in pandas_functions.py.

--- SchemaRefinery/utils/pandas_functions.py
+++ SchemaRefinery/utils/pandas_functions.py
@@ -1,4 +1,17 @@
 import pandas as pd
 
 def dict_to_df(dictionary):
+    """
+    Convert a dictionary to a pandas DataFrame.
+
+    Parameters
+    ----------
+    dictionary : dict
+        The dictionary to convert. Keys are used as column headers.
+
+    Returns
+    -------
+    return : pandas.DataFrame
+        The resulting DataFrame, where each key-value pair in the dictionary corresponds to a column in the DataFrame.
+    """
     return pd.DataFrame.from_dict(dictionary)
