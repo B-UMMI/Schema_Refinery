56d6b7c9ff5792db1b867ab3a633526c4f0e2c01
MForofontov <mykyta.forofontov@hotmail.com>
2024-10-10 09:21:20 +0100

    fix: fixed annotation module

--- SchemaRefinery/SchemaAnnotation/genbank_annotations.py
+++ SchemaRefinery/SchemaAnnotation/genbank_annotations.py
@@ -14,17 +14,19 @@ Code documentation
 
 import os
 import csv
-
+from typing import Dict, List
 from Bio import SeqIO
 
 try:
     from utils import blast_functions as bf
     from utils.sequence_functions import translate_sequence
     from utils import file_functions as ff
+    from utils import linux_functions as lf
 except ModuleNotFoundError:
     from SchemaRefinery.utils import blast_functions as bf
     from SchemaRefinery.utils.sequence_functions import translate_sequence
     from SchemaRefinery.utils import file_functions as ff
+    from SchemaRefinery.utils import linux_functions as lf
 
 
 def get_protein_annotation_fasta(seqRecord):
@@ -71,20 +73,43 @@ def get_protein_annotation_fasta(seqRecord):
 
     return fasta, fasta_dict
 
-
 def genbank_annotations(genbank_files: str, schema_directory: str,
                         output_directory: str, cpu_cores: int,
-                        bsr: float):
+                        bsr: float, translation_table: int) -> str:
+    """
+    Process GenBank files to extract annotations and perform BLAST searches.
 
+    Parameters
+    ----------
+    genbank_files : str
+        Directory containing GenBank files.
+    schema_directory : str
+        Directory containing schema files.
+    output_directory : str
+        Directory to save output files.
+    cpu_cores : int
+        Number of CPU cores to use for BLAST.
+    bsr : float
+        BLAST score ratio threshold.
+
+    Returns
+    -------
+    str
+        Path to the annotations file.
+    """
+    
+    # Create output directory for genbank annotations
     output_directory = os.path.join(output_directory, 'genbank_annotations')
     ff.create_directory(output_directory)
 
-    gbk_files = [os.path.join(genbank_files, f)
-                 for f in os.listdir(genbank_files)]
+    # List and sort GenBank files
+    gbk_files: List[str] = [os.path.join(genbank_files, f) for f in os.listdir(genbank_files)]
     gbk_files.sort()
 
-    fasta = []
-    fasta_dict = {}
+    fasta: List[str] = []
+    fasta_dict: Dict[str, List[str]] = {}
+    
+    # Parse GenBank files and extract protein annotations
     for f in gbk_files:
         recs = [rec for rec in SeqIO.parse(f, 'genbank')]
         for r in recs:
@@ -92,6 +117,7 @@ def genbank_annotations(genbank_files: str, schema_directory: str,
             fasta.extend(outl)
             fasta_dict.update(outd)
 
+    # Save extracted protein sequences to a file
     selected_file = os.path.join(output_directory, 'selected_cds.fasta')
     with open(selected_file, 'w') as outfile:
         fasta_text = '\n'.join(fasta)
@@ -99,17 +125,15 @@ def genbank_annotations(genbank_files: str, schema_directory: str,
 
     # BLAST alleles for each locus against file with all CDSs from origin genomes
     reps_dir = os.path.join(schema_directory, 'short')
-    rep_files = [os.path.join(reps_dir, f)
-                 for f in os.listdir(reps_dir)
-                 if f.endswith('.fasta')]
+    rep_files: List[str] = [os.path.join(reps_dir, f) for f in os.listdir(reps_dir) if f.endswith('.fasta')]
 
     # Get all representative sequences into same file
-    reps = []
-    reps_ids = {}
+    reps: List[str] = []
+    reps_ids: Dict[int, str] = {}
     start = 1
     for f in rep_files:
-        first_rep =  SeqIO.parse(f, 'fasta').__next__()
-        prot = translate_sequence(str(first_rep.seq), 11)
+        first_rep = SeqIO.parse(f, 'fasta').__next__()
+        prot = translate_sequence(str(first_rep.seq), translation_table)
         sequence = '>{0}\n{1}'.format(start, prot)
         reps_ids[start] = first_rep.id
         reps.append(sequence)
@@ -122,7 +146,8 @@ def genbank_annotations(genbank_files: str, schema_directory: str,
 
     # Create BLASTdb
     blastdb_path = os.path.join(output_directory, 'reps_db')
-    bf.make_blast_db(prot_file, blastdb_path, 'prot')
+    makeblastdb_exec = lf.get_tool_path('makeblastdb')
+    bf.make_blast_db(makeblastdb_exec, prot_file, blastdb_path, 'prot')
 
     blastout = os.path.join(output_directory, 'blastout.tsv')
     bf.run_blast('blastp', blastdb_path, selected_file, blastout,
@@ -130,16 +155,16 @@ def genbank_annotations(genbank_files: str, schema_directory: str,
 
     # Import BLAST results
     with open(blastout, 'r') as at:
-        blast_results = list(csv.reader(at, delimiter='\t'))
+        blast_results: List[List[str]] = list(csv.reader(at, delimiter='\t'))
 
     # Convert ids
     for r in blast_results:
         r[1] = reps_ids[int(r[1])]
 
     # Create mapping between sequence IDs and sequence
-    selected_inverse = {v[0]: [k, v[2]] for k, v in fasta_dict.items()}
+    selected_inverse: Dict[str, List[str]] = {v[0]: [k, v[2]] for k, v in fasta_dict.items()}
 
-    best_matches = {}
+    best_matches: Dict[str, List[str]] = {}
     for rec in blast_results:
         try:
             query = rec[0].split('|')[0]
@@ -161,7 +186,7 @@ def genbank_annotations(genbank_files: str, schema_directory: str,
             best_matches[subject] = [query, score, query_name]
 
     # Get identifiers mapping
-    ids_to_name = {v[0]: v[1:] for k, v in fasta_dict.items()}
+    ids_to_name: Dict[str, List[str]] = {v[0]: v[1:] for k, v in fasta_dict.items()}
 
     # Add names
     for k in best_matches:
@@ -175,19 +200,19 @@ def genbank_annotations(genbank_files: str, schema_directory: str,
 
     # Import self results
     with open(reps_blast_out, 'r') as at:
-        reps_blast_results = list(csv.reader(at, delimiter='\t'))
+        reps_blast_results: List[List[str]] = list(csv.reader(at, delimiter='\t'))
 
     # Convert ids
     for r in reps_blast_results:
         r[0] = reps_ids[int(r[0])]
         r[1] = reps_ids[int(r[1])]
 
-    reps_scores = {l[0]: l[-1] for l in reps_blast_results}
+    reps_scores: Dict[str, str] = {l[0]: l[-1] for l in reps_blast_results}
 
     for k in best_matches:
-        best_matches[k].append(float(best_matches[k][1])/float(reps_scores[k]))
+        best_matches[k].append(float(best_matches[k][1]) / float(reps_scores[k]))
 
-    final_best_matches = {k: v for k, v in best_matches.items() if v[5] >= bsr}
+    final_best_matches: Dict[str, List[str]] = {k: v for k, v in best_matches.items() if v[5] >= bsr}
     print('Extracted annotations for {0} loci.'.format(len(final_best_matches)))
 
     # Save annotations
@@ -196,6 +221,6 @@ def genbank_annotations(genbank_files: str, schema_directory: str,
     with open(annotations_file, 'w') as at:
         outlines = [header] + ['{0}\t{1}\t{2}\t{3}\t{4}'.format(k.split("_")[0], v[0], v[3], v[4], v[5]) for k, v in final_best_matches.items()]
         outtext = '\n'.join(outlines)
-        at.write(outtext+'\n')
+        at.write(outtext + '\n')
 
     return annotations_file
