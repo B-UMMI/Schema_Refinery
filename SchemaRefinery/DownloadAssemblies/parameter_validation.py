2f5085a6c2b7fa902e25e1243371b6d96758ed42
MForofontov <mykyta.forofontov@hotmail.com>
2024-11-05 10:51:05 +0000

    feat: Added types

--- SchemaRefinery/DownloadAssemblies/parameter_validation.py
+++ SchemaRefinery/DownloadAssemblies/parameter_validation.py
@@ -8,6 +8,7 @@ import os
 import sys
 import ast
 import csv
+from typing import Any, Dict, List, Optional, Union
 
 try:
     from DownloadAssemblies import constants as ct
@@ -36,38 +37,83 @@ def tryeval(val):
     return val
 
 
-def check_minimum(value, minimum):
-    """Check if a value is below a threshold value."""
-    if value < minimum:
-        return False
-    else:
-        return True
+def check_minimum(value: Union[int, float], minimum: Union[int, float]) -> bool:
+    """Check if a value is below a threshold value.
+
+    Parameters
+    ----------
+    value : Union[int, float]
+        The value to check.
+    minimum : Union[int, float]
+        The minimum threshold value.
+
+    Returns
+    -------
+    bool
+        True if value is above or equal to minimum, False otherwise.
+    """
+    return value >= minimum
 
 
-def check_maximum(value, maximum):
-    """Check if a value is above a threshold value."""
-    if value > maximum:
-        return False
-    else:
-        return True
+def check_maximum(value: Union[int, float], maximum: Union[int, float]) -> bool:
+    """Check if a value is above a threshold value.
+
+    Parameters
+    ----------
+    value : Union[int, float]
+        The value to check.
+    maximum : Union[int, float]
+        The maximum threshold value.
+
+    Returns
+    -------
+    bool
+        True if value is below or equal to maximum, False otherwise.
+    """
+    return value <= maximum
 
 
-def check_value_interval(value, minimum, maximum):
-    """Check if parameter value is contained in interval."""
-    if check_minimum(value) and check_maximum(value):
-        return True
-    else:
-        False
+def check_value_interval(value: Union[int, float], minimum: Union[int, float], maximum: Union[int, float]) -> bool:
+    """Check if parameter value is contained in interval.
+
+    Parameters
+    ----------
+    value : Union[int, float]
+        The value to check.
+    minimum : Union[int, float]
+        The minimum threshold value.
+    maximum : Union[int, float]
+        The maximum threshold value.
+
+    Returns
+    -------
+    bool
+        True if value is within the interval, False otherwise.
+    """
+    return check_minimum(value, minimum) and check_maximum(value, maximum)
 
 
-def check_value_type(value, expected_type):
-    """Check if parameter is of expected type."""
+def check_value_type(value: Any, expected_type: type) -> Optional[Any]:
+    """Check if parameter is of expected type.
+
+    Parameters
+    ----------
+    value : Any
+        The value to check.
+    expected_type : type
+        The expected type of the value.
+
+    Returns
+    -------
+    Optional[Any]
+        The converted value if it matches the expected type, None otherwise.
+    """
     try:
         if expected_type is bool:
             converted = tryeval(value)
         else:
             converted = expected_type(value)
-        if type(converted) is expected_type:
+        if isinstance(converted, expected_type):
             return converted
         else:
             return None
@@ -75,16 +121,37 @@ def check_value_type(value, expected_type):
         return None
 
 
-def check_path(value):
-    """Check if a path exists."""
-    if os.path.exists(value):
-        return True
-    else:
-        return False
+def check_path(value: str) -> bool:
+    """Check if a path exists.
+
+    Parameters
+    ----------
+    value : str
+        The path to check.
+
+    Returns
+    -------
+    bool
+        True if the path exists, False otherwise.
+    """
+    return os.path.exists(value)
 
 
-def check_in_list(values, expected_values):
-    """"""
+def check_in_list(values: List[str], expected_values: List[str]) -> Union[bool, List[str]]:
+    """Check if all values are in the expected values list.
+
+    Parameters
+    ----------
+    values : List[str]
+        The values to check.
+    expected_values : List[str]
+        The list of expected values.
+
+    Returns
+    -------
+    Union[bool, List[str]]
+        The values if all are in the expected values list, False otherwise.
+    """
     intersection = set.intersection(set(expected_values), set(values))
     if len(intersection) < len(values):
         return False
@@ -92,81 +159,92 @@ def check_in_list(values, expected_values):
         return values
 
 
-def check_parameter(value, validate_type, validate_minimum, validate_maximum,
-                    validate_path, validate_list):
-    """Validate a value passed to a parameter."""
-    valid = True
+def check_parameter(value: Any, validate_type: Optional[type], validate_minimum: Optional[Union[int, float]],
+                    validate_maximum: Optional[Union[int, float]], validate_path: bool, validate_list: Optional[List[str]]) -> Union[bool, Any]:
+    """Validate a value passed to a parameter.
+
+    Parameters
+    ----------
+    value : Any
+        The value to validate.
+    validate_type : Optional[type]
+        The expected type of the value.
+    validate_minimum : Optional[Union[int, float]]
+        The minimum threshold value.
+    validate_maximum : Optional[Union[int, float]]
+        The maximum threshold value.
+    validate_path : bool
+        Whether to validate the value as a path.
+    validate_list : Optional[List[str]]
+        The list of expected values.
+
+    Returns
+    -------
+    Union[bool, Any]
+        The validated value if valid, False otherwise.
+    """
+    valid: bool = True
     if validate_type and valid:
         value = check_value_type(value, validate_type)
         if value is None:
             valid = False
     if validate_minimum and valid:
-        valid_min = check_minimum(value, validate_minimum)
-        if valid_min is None:
+        valid_min: bool = check_minimum(value, validate_minimum)
+        if not valid_min:
             valid = False
     if validate_maximum and valid:
-        valid_max = check_maximum(value, validate_maximum)
-        if valid_max is None:
+        valid_max: bool = check_maximum(value, validate_maximum)
+        if not valid_max:
             valid = False
     if validate_path and valid:
-        valid_path = check_path(value)
-        if valid_path is None:
+        valid_path: bool = check_path(value)
+        if not valid_path:
             valid = False
     if validate_list and valid:
         value = check_in_list(value.split(','), validate_list)
         if value is None:
             valid = False
 
-    if valid is None:
-        return valid
-    else:
-        return value
+    return value if valid else False
 
 
-def validate_criteria_file(file_path, expected_criteria=ct.FILTERING_CRITERIA):
-    """
-    Validates intial input criteria arguments file to be according to the desired format.
+def validate_criteria_file(file_path: str, expected_criteria: Dict[str, Any] = ct.FILTERING_CRITERIA) -> Dict[str, Any]:
+    """Validates initial input criteria arguments file to be according to the desired format.
 
-    Parameter
-    ---------
+    Parameters
+    ----------
     file_path : str
         File path to the criteria file.
-    expected_criteria : dict
-        Contains the type and format that criteria file is suposed to have.
+    expected_criteria : Dict[str, Any]
+        Contains the type and format that criteria file is supposed to have.
 
     Returns
     -------
-    parameter_values : dict
+    Dict[str, Any]
         Dictionary that contains the parameters values extracted from criteria file.
     """
     with open(file_path, 'r', encoding='utf-8') as filters:
-        criteria = dict(csv.reader(filters, delimiter='\t'))
+        criteria: Dict[str, str] = dict(csv.reader(filters, delimiter='\t'))
 
-    unexpected_keys = [x
-                       for x in criteria
-                       if x not in expected_criteria]
+    unexpected_keys: List[str] = [x for x in criteria if x not in expected_criteria]
 
-    if len(unexpected_keys) > 0:
+    if unexpected_keys:
         print("\nError: Following unexpected criteria:")
-        unexpected_keys = '\n'.join(unexpected_keys)
-        print(unexpected_keys)
+        print('\n'.join(unexpected_keys))
         sys.exit()
 
-    missing_keys = [x
-                    for x in expected_criteria
-                    if x not in criteria]
+    missing_keys: List[str] = [x for x in expected_criteria if x not in criteria]
 
-    if len(missing_keys) > 0:
+    if missing_keys:
         print("\nError: Missing following criteria:")
-        missing_keys = '\n'.join(missing_keys)
-        print(missing_keys)
+        print('\n'.join(missing_keys))
         sys.exit()
 
-    warnings = []
-    parameter_values = {}
+    warnings: List[str] = []
+    parameter_values: Dict[str, Any] = {}
     for k, v in criteria.items():
         if v not in ['None', '']:
-            valid = check_parameter(v, *ct.CRITERIA_ERRORS[k][1])
+            valid: Union[bool, Any] = check_parameter(v, *ct.CRITERIA_ERRORS[k][1])
             if valid is not None:
                 parameter_values[k] = valid
             else:
@@ -174,8 +252,7 @@ def validate_criteria_file(file_path, expected_criteria=ct.FILTERING_CRITERIA):
         else:
             parameter_values[k] = None
 
-    if len(warnings) > 0:
-        warning_text = '\n'.join(warnings)
-        sys.exit(warning_text)
+    if warnings:
+        sys.exit('\n'.join(warnings))
     else:
         return parameter_values
