1d921fb47347763e0303df254b592d252c4f6400
MForofontov <mykyta.forofontov@hotmail.com>
2024-11-29 13:02:22 +0000

    fix: minor fix to the algorithm

--- SchemaRefinery/utils/core_functions.py
+++ SchemaRefinery/utils/core_functions.py
@@ -1145,9 +1145,9 @@ def write_blast_summary_results(related_clusters: Dict[str, List[Tuple[Any, ...]
         recommendations_report_file.write("Recommendation\tIDs\n")
         for key, recommendation in recommendations.items():
             for category, ids in recommendation.items():
-                category = '_'.join(category.split('_')[:2] if 'Choice' in category else category)
+                category = category.split('_')[:2] if 'Choice' in category else category
                 # If the category is Choice, add the class to the category
-                if len(category) > 1:
+                if isinstance(ids, list):
                     category = '_'.join(category[:2])
                 # Convert all to string
                 ids = itf.convert_set_elements_to_strings(ids)
